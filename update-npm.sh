8c38b2fad9502305f1d90ba08ae7c7c9ee95bff1
Christopher Horrell <christopher@joyent.com>
2015-07-04 00:00:41 -0400

    update Node.js and npm separately
    update.sh now just updates Node.js, update-npm.sh updates npm

--- update-npm.sh
+++ update-npm.sh
@@ -11,16 +11,13 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
+npmVersion="$(npm show npm version 2>/dev/null)"
 for version in "${versions[@]}"; do
 	fullVersion="$(curl -sSL --compressed 'http://nodejs.org/dist' | grep '<a href="v'"$version." | sed -E 's!.*<a href="v([^"/]+)/?".*!\1!' | cut -f 3 -d . | sort -n | tail -1)"
 	(
 		sed -E -i.bak '
-			s/^(ENV NODE_VERSION) .*/\1 '"$version.$fullVersion"'/;
+			s/^(ENV NPM_VERSION) .*/\1 '"$npmVersion"'/;
 		' "$version/Dockerfile" "$version/slim/Dockerfile" "$version/wheezy/Dockerfile"
 		rm $version/Dockerfile.bak $version/slim/Dockerfile.bak $version/wheezy/Dockerfile.bak
-
-		sed -E -i.bak 's/^(FROM node):.*/\1:'"$version.$fullVersion"'/' "$version/onbuild/Dockerfile"
-		rm $version/onbuild/Dockerfile.bak
-
 	)
 done

67ec55de7f73825a64be80f5af3440d6b0317752
Christopher Horrell <christopher@horrell.ca>
2016-03-16 21:41:10 -0400

    Update for 5.9.0

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -5,7 +5,7 @@ hash git 2>/dev/null || { echo >&2 "git not found, exiting."; }
 
 array_0_12='0';
 array_4_4='4 argon';
-array_5_8='5 latest';
+array_5_9='5 latest';
 
 cd $(cd ${0%/*} && pwd -P);
 

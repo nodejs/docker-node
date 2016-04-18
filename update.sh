4285b7a836cdfe4d65e6d56f1bab2f258d2021c7
Christopher Horrell <christopher@horrell.ca>
2016-04-18 11:00:06 -0400

    Don't set npm log info for 0.10 and 0.12.
    Reference: #57, #144

--- update.sh
+++ update.sh
@@ -28,6 +28,12 @@ for version in "${versions[@]}"; do
 			cp $template $dockerfile
 			sed -E -i.bak 's/^(ENV NODE_VERSION |FROM node:).*/\1'"$version.$fullVersion"'/' "$dockerfile"
 			rm "$dockerfile.bak"
+			
+			# Don't set npm log level in 0.10 and 0.12.
+			if [[ "$version" == "0.10" || "$version" == "0.12" ]]; then
+				sed -E -i.bak '/^ENV NPM_CONFIG_LOGLEVEL info/d' "$dockerfile"
+				rm "$dockerfile.bak"
+			fi
 
 		)
 	done

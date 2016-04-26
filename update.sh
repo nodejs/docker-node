e541be578fe985651e9cd20e0ed47882623b528b
Christopher Horrell <christopher@horrell.ca>
2016-04-26 17:04:04 -0400

    Fix update.sh to account for default Dockerfile
    Fixes #170

--- update.sh
+++ update.sh
@@ -9,10 +9,34 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
+
+template=
+dockerfile=
+
+function update_node_version {
+	fullVersion="$(curl -sSL --compressed 'https://nodejs.org/dist' | grep '<a href="v'"$version." | sed -E 's!.*<a href="v([^"/]+)/?".*!\1!' | cut -f 3 -d . | sort -n | tail -1)"
+	(
+		cp $template $dockerfile
+		sed -E -i.bak 's/^(ENV NODE_VERSION |FROM node:).*/\1'"$version.$fullVersion"'/' "$dockerfile"
+		rm "$dockerfile.bak"
+
+		# Don't set npm log level in 0.10 and 0.12.
+		if [[ "$version" == "0.10" || "$version" == "0.12" ]]; then
+			sed -E -i.bak '/^ENV NPM_CONFIG_LOGLEVEL info/d' "$dockerfile"
+			rm "$dockerfile.bak"
+		fi
+	)
+}
+
 for version in "${versions[@]}"; do
 	if [[ "$version" == "docs" ]]; then
 		continue
 	fi
+
+	template="Dockerfile.template"
+	dockerfile="$version/Dockerfile"
+
+	update_node_version
 
 	variants=$(ls -d $version/*/ | awk -F"/" '{print $2}')
 
@@ -20,23 +44,7 @@ for version in "${versions[@]}"; do
 		template="Dockerfile-$variant.template"
 		dockerfile="$version/$variant/Dockerfile"
 
-		if [[ "$variant" == "default" ]]; then
-			template="Dockerfile.template"
-			dockerfile="$version/Dockerfile"
-		fi
+		update_node_version
 
-		fullVersion="$(curl -sSL --compressed 'https://nodejs.org/dist' | grep '<a href="v'"$version." | sed -E 's!.*<a href="v([^"/]+)/?".*!\1!' | cut -f 3 -d . | sort -n | tail -1)"
-		(
-			cp $template $dockerfile
-			sed -E -i.bak 's/^(ENV NODE_VERSION |FROM node:).*/\1'"$version.$fullVersion"'/' "$dockerfile"
-			rm "$dockerfile.bak"
-
-			# Don't set npm log level in 0.10 and 0.12.
-			if [[ "$version" == "0.10" || "$version" == "0.12" ]]; then
-				sed -E -i.bak '/^ENV NPM_CONFIG_LOGLEVEL info/d' "$dockerfile"
-				rm "$dockerfile.bak"
-			fi
-
-		)
 	done
 done

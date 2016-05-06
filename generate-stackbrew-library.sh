a385a80fb444c5c3a3ffd1ca4c6a8aa8ea71e79c
Christopher Horrell <christopher@joyent.com>
2016-05-05 20:42:13 -0400

    v6.1.0 is now the latest

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -6,7 +6,7 @@ hash git 2>/dev/null || { echo >&2 "git not found, exiting."; }
 array_0_12='0';
 array_4_4='4 argon';
 array_5_11='5';
-array_6_0='6 latest';
+array_6_1='6 latest';
 
 cd $(cd ${0%/*} && pwd -P);
 

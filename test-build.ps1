e4f9a12d1dc34f35fe12ea15f59edb5d8c85e1d9
Stefan Scherer <scherer_stefan@icloud.com>
2016-08-25 16:32:08 +0200

    Introduce Dockerfiles for windowsservercore
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

---
+++ test-build.ps1
@@ -0,0 +1,8 @@
+docker build -t node:4.7.0-windowsservercore 4.7/windows/windowsservercore
+docker build -t node:4.7.0-windowsservercore-onbuild 4.7/windows/windowsservercore/onbuild
+
+docker build -t node:6.9.2-windowsservercore 6.9/windows/windowsservercore
+docker build -t node:6.9.2-windowsservercore-onbuild 6.9/windows/windowsservercore/onbuild
+
+docker build -t node:7.2.1-windowsservercore 7.2/windows/windowsservercore
+docker build -t node:7.2.1-windowsservercore-onbuild 7.2/windows/windowsservercore/onbuild

910443c39c80291f0bf24712d8d94279cf15b7b5
Christopher Horrell <christopher@joyent.com>
2016-08-29 16:24:46 -0400

    Update for Node.js v6.5.0

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -6,7 +6,7 @@ hash git 2>/dev/null || { echo >&2 "git not found, exiting."; }
 array_0_12='0';
 array_4_5='4 argon';
 array_5_12='5';
-array_6_4='6 latest';
+array_6_5='6 latest';
 
 cd $(cd ${0%/*} && pwd -P);
 

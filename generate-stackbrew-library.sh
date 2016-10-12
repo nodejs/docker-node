c517001982231c78bf4ce757466c9b69d02c92af
Christopher Horrell <christopher@joyent.com>
2016-10-12 17:11:27 -0400

    Add missing update for generate-stackbrew-library.sh

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -5,7 +5,7 @@ hash git 2>/dev/null || { echo >&2 "git not found, exiting."; }
 
 array_0_12='0';
 array_4_6='4 argon';
-array_6_7='6 latest';
+array_6_8='6 latest';
 
 cd $(cd ${0%/*} && pwd -P);
 

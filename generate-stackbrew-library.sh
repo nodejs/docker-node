655aea9585318c5a0509fa673f989c3b7d9df7ee
Christopher Horrell <christopher@joyent.com>
2016-11-09 10:26:08 -0500

    Node.js v7.1.0 is now the latest
    Updates generate-stackbrew-library.sh

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -6,7 +6,7 @@ hash git 2>/dev/null || { echo >&2 "git not found, exiting."; }
 array_0_12='0';
 array_4_6='4 argon';
 array_6_9='6 boron';
-array_7_0='7 latest';
+array_7_1='7 latest';
 
 cd $(cd ${0%/*} && pwd -P);
 

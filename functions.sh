bc4e7eecfe9516e7fd42b8d951e30855ee614eff
Yihong Wang <yh.wang@ibm.com>
2017-07-23 00:24:35 -0700

    Generate 'Architectures' information (#472)
    Enhance the `generate-stackbrew-library.sh` to generate `Architectures`
    information.
    
    Signed-off-by: Yihong Wang <yh.wang@ibm.com>

--- functions.sh
+++ functions.sh
@@ -11,7 +11,7 @@ function get_arch() {
 	local arch
 	case $(uname -m) in
 	x86_64)
-		arch="x64"
+		arch="amd64"
 		;;
 	ppc64le)
 		arch="ppc64le"
@@ -39,4 +39,28 @@ function get_variants() {
 	local variants
 	variants=$(grep "$arch" architectures | sed -E 's/'"$arch"'\s*//' | sed -E 's/,/ /g')
 	echo "$variants"
+}
+
+# Get supported architectures for a specific version and variant
+#
+# Get default supported architectures from 'architectures'. Then go to the version folder
+# to see if there is a local architectures file. The local architectures will override the
+# default architectures. This will give us some benefits:
+# - a specific version may or may not support some architectures
+# - if there is no specialization for a version, just don't provide local architectures
+function get_supported_arches () {
+	local version
+	local variant
+	local arches
+	version="$1"; shift
+	variant="$1"; shift
+
+	# Get default supported arches
+	arches=$( grep "$variant" architectures 2>/dev/null | cut -d' ' -f1 )
+
+	# Get version specific supported architectures if there is specialized information
+	if [ -a "$version"/architectures ]; then
+		arches=$( grep "$variant" "$version"/architectures 2>/dev/null | cut -d' ' -f1 )
+	fi
+	echo "$arches"
 }

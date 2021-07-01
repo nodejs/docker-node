cc26eb1fa6cd9f52db56125ec01e5e41638ad7ec
Nick Schonning <nschonni@gmail.com>
2021-07-01 13:14:24 -0400

    chore: Use release-keys CLI

--- update.sh
+++ update.sh
@@ -146,7 +146,7 @@ function update_node_version() {
 '
 
     # Add GPG keys
-    for key_type in "node" "yarn"; do
+    for key_type in "yarn"; do
       while read -r line; do
         pattern='"\$\{'$(echo "${key_type}" | tr '[:lower:]' '[:upper:]')'_KEYS\[@\]\}"'
         sed -Ei -e "s/([ \\t]*)(${pattern})/\\1${line}${new_line}\\1\\2/" "${dockerfile}-tmp"

ae95adfa3f6e55fb083dbf5baad6f68f12e9e0bb
Pedro Henrique <pedro.silva@wisenet.inf.br>
2022-03-06 12:06:27 -0300

    requested changes pt 3

--- build-automation.mjs
+++ build-automation.mjs
@@ -9,13 +9,9 @@ const exec = promisify(child_process.exec);
 // and returns whether we should update or not
 const checkIfThereAreNewVersions = async () => {
   try {
-    const nodeWebsiteText = await (await fetch('https://nodejs.org/en/download/releases/')).text();
-
     const { stdout: versionsOutput } = await exec(". ./functions.sh && get_versions", { shell: "bash" });
 
     const supportedVersions = versionsOutput.trim().split(" ");
-
-    const availableVersions = nodeWebsiteText.match(new RegExp("Node\\.js (" + supportedVersions.join('|') + ")\\.\\d+\\.\\d+", "g"));
 
     let lsOutput = "";
     let latestSupportedVersions = {};
@@ -30,21 +26,21 @@ const checkIfThereAreNewVersions = async () => {
       latestSupportedVersions[supportedVersion] = { fullVersion: fullVersionOutput.trim() };
     }
 
-    // filter only more recent versions of availableVersions for each major version in latestSupportedVersions' keys
+    const availableVersionsJson = await (await fetch('https://nodejs.org/download/release/index.json')).json();
+
+    // filter only more recent versions of availableVersionsJson for each major version in latestSupportedVersions' keys
     // e.g. if latestSupportedVersions = { "12": "12.22.10", "14": "14.19.0", "16": "16.14.0", "17": "17.5.0" }
     // and availableVersions = ["Node.js 12.22.10", "Node.js 12.24.0", "Node.js 14.19.0", "Node.js 14.22.0", "Node.js 16.14.0", "Node.js 16.16.0", "Node.js 17.5.0", "Node.js 17.8.0"]
     // return { "12": "12.24.0", "14": "14.22.0", "16": "16.16.0", "17": "17.8.0" }
 
     let filteredNewerVersions = {};
 
-    for (let availableVersion of availableVersions) {
-      if (availableVersion.includes("Node.js ")) {
-        const [availableMajor, availableMinor, availablePatch] = availableVersion.split(" ")[1].split(".");
-        const [_latestMajor, latestMinor, latestPatch] = latestSupportedVersions[availableMajor].fullVersion.split(".");
-        if (latestSupportedVersions[availableMajor] && (Number(availableMinor) > Number(latestMinor) || (availableMinor === latestMinor && Number(availablePatch) > Number(latestPatch)))) {
-          filteredNewerVersions[availableMajor] = { fullVersion: `${availableMajor}.${availableMinor}.${availablePatch}` };
-          continue
-        }
+    for (let availableVersion of availableVersionsJson) {
+      const [availableMajor, availableMinor, availablePatch] = availableVersion.version.split("v")[1].split(".");
+      const [_latestMajor, latestMinor, latestPatch] = latestSupportedVersions[availableMajor].fullVersion.split(".");
+      if (latestSupportedVersions[availableMajor] && (Number(availableMinor) > Number(latestMinor) || (availableMinor === latestMinor && Number(availablePatch) > Number(latestPatch)))) {
+        filteredNewerVersions[availableMajor] = { fullVersion: `${availableMajor}.${availableMinor}.${availablePatch}` };
+        continue
       }
     }
 
@@ -87,15 +83,17 @@ if (!shouldUpdate) {
   process.exit(0);
 } else {
   const newVersions = await checkForMuslVersionsAndSecurityReleases(versions);
-  for(let version of Object.keys(newVersions)) {
+  let updatedVersions = [];
+  for (let version of Object.keys(newVersions)) {
     if (newVersions[version].muslBuildExists) {
       let { stdout } = await exec(`./update.sh ${newVersions[version].isSecurityRelease ? "-s " : ""}${version}`);
       console.log(stdout);
-      await exec(`echo "${newVersions[version].fullVersion} " >> updated_versions`);
+      updatedVersions.push(newVersions[version].fullVersion);
     } else {
       console.log(`There's no musl build for version ${newVersions[version].fullVersion} yet.`);
     }
   };
+  console.log(`::set-output name=updated-versions::${updatedVersions.join(',')}`);
   let stdout = (await exec(`git diff`)).stdout;
   console.log(stdout);
 }
